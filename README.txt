5db180cb7c54d83d1bdef9719bde05a1e05088a2
Adriano Ayala <adriano.ayc@gmail.com>
2026-06-03 00:23:18 -0400

    NatsModule - Modulo personalizado

--- README.txt
+++ README.txt
@@ -41,6 +41,8 @@ CLIENT GATEWAY (Manejo de errores y validación)
             $ nest g res orders --no-spec
                 > ? What transport layer do you use? REST API
                 > ? Would you like to generate CRUD entry points? (Y/n) y
+        + Crear un nuevo modulo (Rename folder to 'transports')
+            $ nest g mo nats
 
     - Postman
         + Crete a new Workspace (Click "New" | "Workspace" > "Blank workspace")
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,23 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 import { ProductsController } from './products.controller';
-import { envs, NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   imports: [
-    // Registar el servicio (product-ms)
-    ClientsModule.register([
-      {
-        name: NATS_SERVICE,
-        transport: Transport.NATS, // Protocolo de transporte
-        options: {
-          servers: envs.natsServers, // URL del servidor NATS
-        },
-      },
-    ]),
+    // NATS module
+    NatsModule,
   ],
 })
 export class ProductsModule {}
